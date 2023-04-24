c601832c7b014131e6c307d4b040783415f6cffc
Brian-Enos <brianenos9199@gmail.com>
2023-04-24 13:17:21 -0700

    Made some changse to the header file

--- main.h
+++ main.h
@@ -1,23 +1,35 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
-#include <sys/wait.h>
-#include <errno.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <string.h>
 #include <signal.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
 
-#define MAX_INPUT_LENGTH 1024
-#define MAX_ARGS 64
+/* function prototypes */
+int _strcmp(char *s1, char *s2);
+char *_strcat(char *dest, char *src);
+char *_strdup(char *str);
+int _strlen(char *s);
+char **_strtok(char *str, char *delim);
+char *_getenv(char *name);
+int execute(char **args, char **env);
+char *getpath(char **env);
+void sig_handler(int signum);
+void prompt(void);
 
-void handle_sigint(int sig);
-void print_prompt();
-int parse_input(char* input, char** args);
-void execute_command(char** args);
-/*
-* The end of main.h file
-*/
-#endif 
+/* helper functions */
+int count_words(char *str, char *delim);
+void free_args(char **args);
+int check_path(char **args, char **env, char *command);
+int check_builtins(char **args, char **env);
+void print_env(char **env);
+
+#endif /* MAIN_H */
 
