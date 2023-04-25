e5cc57fd475e8ec0c224a8c4b17ba127c06f37b8
Brian Enos Otieno <brianenos9199@gmail.com>
2023-04-25 10:47:35 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,6 +13,7 @@ void handle_sigint(int sig);
 void print_prompt();
 int parse_input(char* input, char** args);
 void execute_command(char** args);
+void free_args(char** args, int num_args);
 
 int main(int argc, char** argv) {
     char input[MAX_INPUT_LENGTH];
@@ -56,9 +58,7 @@ int main(int argc, char** argv) {
         /* 
         *Free memory allocated for argument strings
         */
-        for (int i = 0; i < num_args; i++) {
-            free(args[i]);
-        }
+        free_args(args, num_args);
     }
 
     return 0;
@@ -112,104 +112,106 @@ int parse_input(char* input, char** args) {
     return num_args;
 }
     
-    void execute_command(char** args) {
+void execute_command(char** args) {
     pid_t pid = fork(); /* 
-                         *Create a child process
+                         *Create a child proces
                          */
-    if (pid == -1) {
-        perror("fork"); /* 
-                         * Print an error message if fork() fails
-                         */
-        exit(EXIT_FAILURE);
-    } else if (pid == 0) {
-        /* This is the child process
-        *
-        */
-        int ret = execvp(args[0], args); // Execute the command
-        if (ret == -1) {
-            perror("execvp"); /* 
-                               * Print an error message if execvp() fails
-                               */
-            exit(EXIT_FAILURE);
-        }
-    } else {
-        /*
-        * This is the parent process
-        */
-        int status;
-        waitpid(pid, &status, 0); /* 
-                                   * Wait for the child process to complete
-                                   */
-    }
+if (pid == -1) {
+perror("fork"); /
+* Print an error message if fork() fails
+/
+exit(EXIT_FAILURE);
+} else if (pid == 0) {
+/ This is the child process
+*
+/
+int ret = execvp(args[0], args); // Execute the command
+if (ret == -1) {
+perror("execvp"); /
+* Print an error message if execvp() fails
+/
+exit(EXIT_FAILURE);
+}
+} else {
+/
+* This is the parent process
+/
+int status;
+waitpid(pid, &status, 0); /
+* Wait for the child process to complete
+*/
+}
+free_args(args, MAX_ARGS);
 }
 
 void handle_sigint(int sig) {
-    printf("\n");
-    print_prompt();
+printf("\n");
+print_prompt();
 }
 
 void print_prompt() {
-    printf("$ ");
-    fflush(stdout);
+printf("$ ");
+fflush(stdout);
 }
 
 int parse_input(char* input, char** args) {
-    int num_args = 0;
-    char* token = strtok(input, " ");
-    while (token != NULL && num_args < MAX_ARGS) {
-        args[num_args] = malloc(strlen(token) + 1);
-        strcpy(args[num_args], token);
-        num_args++;
-        token = strtok(NULL, " ");
-    }
-    args[num_args] = NULL;
-    return num_args;
+int num_args = 0;
+char* token = strtok(input, " ");
+while (token != NULL && num_args < MAX_ARGS) {
+args[num_args] = malloc(strlen(token) + 1);
+strcpy(args[num_args], token);
+num_args++;
+token = strtok(NULL, " ");
+}
+args[num_args] = NULL;
+return num_args;
 }
 
 void free_args(char** args, int num_args) {
-    for (int i = 0; i < num_args; i++) {
-        free(args[i]);
-    }
+for (int i = 0; i < num_args; i++) {
+free(args[i]);
+}
 }
 
 int main(int argc, char** argv) {
-    char input[MAX_INPUT_LENGTH];
-    char* args[MAX_ARGS];
-    int status;
+char input[MAX_INPUT_LENGTH];
+char* args[MAX_ARGS];
+int status;
+/*
+* Register a signal handler for SIGINT (interrupt signal)
+*/
+signal(SIGINT, handle_sigint);
 
-    /*
-    * Register a signal handler for SIGINT (interrupt signal)
-    */
-    signal(SIGINT, handle_sigint);
-
-    while (1) {
-        print_prompt();
-        if (fgets(input, MAX_INPUT_LENGTH, stdin) == NULL) {
-            if (feof(stdin)) {
-                printf("Exiting...\n");
-                exit(EXIT_SUCCESS);
-            } else {
-                perror("fgets");
-                exit(EXIT_FAILURE);
-            }
+while (1) {
+    print_prompt();
+    if (fgets(input, MAX_INPUT_LENGTH, stdin) == NULL) {
+        if (feof(stdin)) {
+            printf("Exiting...\n");
+            exit(EXIT_SUCCESS);
+        } else {
+            perror("fgets");
+            exit(EXIT_FAILURE);
         }
-        input[strcspn(input, "\n")] = '\0';
+    }
+    input[strcspn(input, "\n")] = '\0';
 
-        /* 
-        *Parse the input into individual arguments
-        */
-        int num_args = parse_input(input, args);
-
-        /*
-        * Execute the command
-        */
-        execute_command(args);
-
-        /* 
-        *Free memory allocated for argument strings
-        */
-        free_args(args, num_args);
+    /* 
+    *Parse the input into individual arguments
+    */
+    int num_args = parse_input(input, args);
+    if (num_args == -1) {
+        fprintf(stderr, "Error: Too many arguments\n");
+        continue;
+    } else if (num_args == -2) {
+        fprintf(stderr, "Error: Empty command\n");
+        continue;
     }
 
-    return 0;
+    /*
+    * Execute the command
+    */
+    execute_command(args);
+}
+
+return 0;
 }
