847866c3e4e247d68d35da7256207c2ae17a166d
Brian Enos Otieno <brianenos9199@gmail.com>
2023-04-25 10:16:56 +0300

    Update main.c

--- main.c
+++ main.c
@@ -38,43 +38,63 @@ int main(int argc, char** argv) {
 
 void handle_sigint(int sig) {
     printf("\n"); / Print a newline character
-    print_prompt(); // Print the shell prompt
+    print_prompt(); /*
+                    * Printing the shell prompt
+                    */
 }
 
 void print_prompt() {
-    printf("$ "); // Print the shell prompt
-    fflush(stdout); // Flush stdout to ensure prompt is printed immediately
+    printf("$ "); /* 
+                   * Print the shell prompt
+                   */
+    fflush(stdout); /* 
+                    * Flush stdout to ensure prompt is printed immediately
+                    */
 }
 
 int parse_input(char* input, char** args) {
     int num_args = 0;
     char* token = strtok(input, " ");
     while (token != NULL && num_args < MAX_ARGS) {
-        args[num_args] = malloc(strlen(token) + 1); // Allocate memory for argument string
+        args[num_args] = malloc(strlen(token) + 1); /*
+                                                     * Allocate memory for argument string
+                                                     */
         strcpy(args[num_args], token);
         num_args++;
         token = strtok(NULL, " ");
     }
-    args[num_args] = NULL; // Add a NULL terminator to the argument list
+    args[num_args] = NULL; /* 
+                            * Add a NULL terminator to the argument list
+                            */
     return num_args;
 }
 
 void execute_command(char** args) {
-    pid_t pid = fork(); // Create a child process
+    pid_t pid = fork(); /* 
+                         *Create a child process
+                         */
     if (pid == -1) {
-        perror("fork"); // Print an error message if fork() fails
+        perror("fork"); /* 
+                         * Print an error message if fork() fails
+                         */
         exit(EXIT_FAILURE);
     } else if (pid == 0) {
-        // This is the child process
+        /* This is the child process
+        *
+        */
         int ret = execvp(args[0], args); // Execute the command
         if (ret == -1) {
             perror("execvp"); // Print an error message if execvp() fails
             exit(EXIT_FAILURE);
         }
     } else {
-        // This is the parent process
+        /*
+        * This is the parent process
+        */
         int status;
-        waitpid(pid, &status, 0); // Wait for the child process to complete
+        waitpid(pid, &status, 0); /* 
+                                   * Wait for the child process to complete
+                                   */
     }
 }
 
