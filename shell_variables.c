7c1b99c7697a8e34b57c42b832449c7618831552
Brian Enos Otieno <brianenos9199@gmail.com>
2023-04-26 12:35:44 +0300

    updated

--- shell_variables.c
+++ shell_variables.c
@@ -1,93 +1,12 @@
-#include "main.h"
+#!/bin/bash
 
-int status;
-int line_num;
-char *nameOfShell;
+while true; do
+  read -p "$ " input
 
-char *checkIfArgumentIsVariable(char *arg)
-{
-	char *clone = NULL;
-	char *pointer = arg;
-	char *nextNode;
-	char *temporary;
-	int is_var;
-	int i;
+  # Handle variable replacement
+  input=${input//\$\$/$$} # Replace $$ with the process ID
+  input=${input//\$\?/$?} # Replace $? with the exit status of the last command
 
-	while (*pointer != '\0')
-	{
-		if (*pointer == '$')
-		{
-			if (clone == NULL)
-			{
-				clone = copyString(arg);
-        
-				i = pointer - arg;
-        
-				pointer = clone + i;
-			}
-			nextNode = pointer + 1;
-			while (*nextNode != '\0' && *nextNode != '$' && *nextNode != '#')
-				nextNode++;
-			if (*nextNode == '$' && nextNode > pointer + 1)
-				is_var = POSITIVE;
-			else if (*nextNode == '#')
-				is_var = NONE;
-			else
-				is_var = NEGATIVE;
-
-			*nextNode = '\0';
-
-			if (compareString("$?", pointer, EQUAL) == POSITIVE)
-				temporary = _itoa(status);
-      
-			else if (compareString("$0", pointer, EQUAL) == POSITIVE)
-				temporary = copyString(nameOfShell);
-      
-			else if (findArrayElement(environ, pointer + 1) != NULL)
-				temporary = copyString(findArrayElement
-                               
-					      (environ, pointer + 1)
-                               
-					      + getStringLength(pointer));
-			else
-				temporary = copyString("");
-
-			*pointer = '\0';
-			pointer = string_concat(clone, temporary);
-      
-			free(temporary);
-			if (is_var == NEGATIVE)
-			{
-				free(clone);
-				clone = pointer;
-				break;
-			}
-			if (is_var == POSITIVE)
-				*nextNode = '$';
-			else if (is_var == NONE)
-				*nextNode = '#';
-			temporary = string_concat(pointer, nextNode);
-			free(pointer);
-			pointer = temporary;
-			free(clone);
-			clone = pointer;
-			if (is_var == NONE)
-			{
-				while (*pointer != '#')
-					pointer++;
-			}
-		}
-    
-    
-    
-		pointer++;
-    
-	}
-	if (clone != NULL)
-		return (clone);
-  
-  
-  
-
-	return (arg);
-}
+  # Execute command
+  eval "$input"
+done
