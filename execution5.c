783a04078845add5633bd3f42a0e1b80299a6979
Brian Enos Otieno <brianenos9199@gmail.com>
2023-04-26 11:05:05 +0300

    fifth execution file updated

---
+++ execution5.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * @pathstr: The PATH string.
+ * @cmd: The cmd to find.
+ * @info: The info struct.
+ * Return: Full path of cmd if found or NULL.
+ * find_path - Finds this cmd in the PATH string.
+ */
+char *find_path(info_t *info, char *pathstr, char *cmd)
+{
+	int i = 0, curr_pos = 0;
+	char *path;
+
+	if (!pathstr)
+		return (NULL);
+	if ((_strlen(cmd) > 2) && starts_with(cmd, "./"))
+	{
+		if (is_cmd(info, cmd))
+			return (cmd);
+	}
+	while (1)
+	{
+		if (!pathstr[i] || pathstr[i] == ':')
+		{
+			path = dup_chars(pathstr, curr_pos, i);
+			if (!*path)
+				_strcat(path, cmd);
+			else
+			{
+				_strcat(path, "/");
+				_strcat(path, cmd);
+			}
+			if (is_cmd(info, path))
+				return (path);
+			if (!pathstr[i])
+				break;
+			curr_pos = i;
+		}
+		i++;
+	}
+	return (NULL);
+}
