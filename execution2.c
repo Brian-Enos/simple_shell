e7ad2ad0650610d147d1e28dbeb79cf5affb3fcd
Brian Enos Otieno <brianenos9199@gmail.com>
2023-04-26 10:58:25 +0300

    Updated the contents of the execution file

---
+++ execution2.c
@@ -0,0 +1,146 @@
+#include "main.h"
+
+
+void free_info(info_t *info, int all)
+{
+	ffree(info->argv);
+	info->argv = NULL;
+	info->path = NULL;
+
+	if (all)
+	{
+		if (info->cmd_buf == NULL)
+			free(info->arg);
+		if (info->env)
+			free_list(&(info->env));
+		if (info->history)
+			free_list(&(info->history));
+		if (info->alias)
+			free_list(&(info->alias));
+		ffree(info->environ);
+			info->environ = NULL;
+		bfree((void **) info->cmd_buf);
+
+		if (info->readfd > 2)
+			close(info->readfd);
+
+		_putchar(BUF_FLUSH);
+	}
+}
+
+
+
+
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	while (src[i] != '\0' && i < n - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	if (i < n)
+	{
+		j = i;
+		while (j < n)
+		{
+			dest[j] = '\0';
+			j++;
+		}
+	}
+
+	return (s);
+}
+
+
+
+
+
+
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	j = 0;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	if (j < n)
+	{
+		dest[i] = '\0';
+	}
+
+	return (s);
+}
+
+
+
+
+char *_strchr(char *s, char c)
+{
+	do
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+	}
+	while (*s++ != '\0');
+
+	return (NULL);
+}
+
+
+
+
+
+ssize_t input_buf(info_t *info, char **buf, size_t *len)
+{
+	ssize_t r = 0;
+	size_t len_p = 0;
+
+	if (!*len)
+	{
+		free(*buf);
+		*buf = NULL;
+		signal(SIGINT, sigintHandler);
+#if USE_GETLINE
+		r = getline(buf, &len_p, stdin);
+#else
+		r = _getline(info, buf, &len_p);
+#endif
+		if (r > 0)
+		{
+			if ((*buf)[r - 1] == '\n')
+			{
+				(*buf)[r - 1] = '\0';
+				r--;
+			}
+			info->linecount_flag = 1;
+			remove_comments(*buf);
+			build_history_list(info, *buf, info->histcount++);
+			{
+				*len = r;
+				info->cmd_buf = buf;
+			}
+		}
+	}
+	return (r);
+}
