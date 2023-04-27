535facdba2ffba9fd721661c63ac70c34103df87
Brian Enos Otieno <brianenos9199@gmail.com>
2023-04-27 16:34:18 +0300

    more updates to the builtin file

--- builtin.c
+++ builtin.c
@@ -1,5 +1,7 @@
 #include "shell.h"
 
+
+
 /**
  * _myexit - exits the shell
  * @info: Structure containing potential arguments. Used to maintain
@@ -7,6 +9,10 @@
  *  Return: exits with a given exit status
  *         (0) if info.argv[0] != "exit"
  */
+
+
+
+
 int _myexit(info_t *info)
 {
 	int exitcheck;
@@ -28,6 +34,9 @@ int _myexit(info_t *info)
 	info->err_num = -1;
 	return (-2);
 }
+
+
+
 
 /**
  * _mycd - changes the current directory of the process
@@ -79,19 +88,24 @@ int _mycd(info_t *info)
 	return (0);
 }
 
+
+
+
+
+
+
 /**
  * _myhelp - changes the current directory of the process
  * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
+ * constant function prototype.
  *  Return: Always 0
  */
 int _myhelp(info_t *info)
 {
 	char **arg_array;
-
 	arg_array = info->argv;
 	_puts("help call works. Function not yet implemented \n");
 	if (0)
-		_puts(*arg_array); /* temp att_unused workaround */
+		_puts(*arg_array);
 	return (0);
 }
