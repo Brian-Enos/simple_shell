95b959c5ad3063b7a370f0e4def8944c78197896
Brian Enos Otieno <brianenos9199@gmail.com>
2023-04-27 10:27:45 +0300

    updates

---
+++ shell.h
@@ -0,0 +1,15 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#define MAXIMUM_ARGUEMENTS 10
+#define BUFFER_SIZE 1024
+
+int main(void);
+
+#endif
